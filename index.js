330f92a7632af5baaab5c8c4d2d61f97f6270bc4
Rakshitha B A <rakshitha.aruna@gmail.com>
2021-09-26 11:36:33 +0530

    Project Done

---
+++ index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    let colors = ['primary', 'danger', 'warning', 'info', 'dark', 'secondary', 'success'];
+    for(let i=0;i<8;i++){
+        for(let j=0;j<3;j++){
+            $("#mainDiv").append('<div class="p-3 mb-2 bg-' + colors[i] + '"></div>');
+        }
+    }
+    $("#color").keyup(function(){
+        let text = this.value;
+        let color = new RegExp(text);
+        if(color !== ""){
+            for(let i=0;i<7;i++){
+                if(color.test(colors[i]) === true){
+                    $(".bg-"+colors[i]).css("display","block");
+                }
+                else{
+                    $(".bg-"+colors[i]).css("display","none");
+                }
+            }
+        }
+    })
+})
